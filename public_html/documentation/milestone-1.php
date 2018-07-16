8d1741aff4f0abf19fbbdc694b1fd536f114b7b4
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-15 21:25:45 -0600

    Created goals and purpose for PWP. Added persona.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,27 @@
+<!DOCTYYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Sarah Heckendorn</title>
+	</head>
+	<body>
+		<h2>Purpose, Audience, Goals</h2>
+		<div>
+			<p><strong>Purpose:</strong>The purpose of this project will be to present an online portfolio with a focus on front-end development.</p>
+			<p><strong>Audience:</strong>My target audience will be professionals in the I.T. and web development community in the local area, focusing mainly on: hiring managers, startup founders, recruiters and peers.</p>
+			<p><strong>Goals:</strong>The primary goal of this website is for potential employees to quickly contact me with ease through my LinkdIn accounts and on site contact form. The secondary goal is to present and showcase my skills, abilities, and work.</p>
+		</div>
+		<h2>Persona</h2>
+		<div>
+			<p><strong>Name:</strong> Greg Talbot
+				</br>
+			<strong>Age:</strong> 39</p>
+			<p><strong>Profession:</strong> Founder of Trackit.com, a professional company designed to help small businesses in the local area. Their product helps owners manage back office inventory, sales, profits, losses, and more. Greg is currently looking for an individual that is skilled in both front-end and back-end web development and has a fresh look on the community to help improve their product.</p>
+			<p><strong>Technology:</strong> Dell User; uses Dell PowerEdge T640 Tower Server with 2 Dell 22 Monitor P2217H at the office, a Dell Inspiron 17R Intel Core i5-4200U Processor 1.6 GHz at home, and a Samsung Galaxy s7 Qualcomm Snapdragon 820 Quad Core 2160.00 MHz</p>
+			<p><strong>Attitudes and Behaviours:</strong> While Greg most certainly knows how to get his work done in order to keep up with the quickly growing community of service providers, he prefers to work with slow diligence to ensure better quality services to his clients. That being said, he is a huge advocate of taking time to grow networks and get to know other developers in the community. He would like to be able to quickly see the work of applicants for the job to see if they seem qualified well enough to then schedule a sit down, face to face interview. Though he won't spend much time looking over the portfolio and resume, he does want to have time to personally assess the candidates to see if they will be a good fit for his company. He will also seek out others in his networking circles to find out more about the applicant's attitude and personality as he strongly believes more in the word of a trusted individual than a website or account.</p>
+			<p><strong>Frustrations and Needs:</strong> Greg needs to be able to hire an individual who has 2-3 years of experience and is willing to jump on board with the vision of the company. He is also looking over new-to-the-industry candidates who present knowledgable skills and good experience with a strong sense of learning on the go. Greg needs a fresh pair of eyes to give some new insights to his company on how they can better help the local businesses. Although he might prefer to sit down for a personal interview, Greg is busy running his business at full speed. He won't pick candidates for interviews until he has briefly looked over their github and linkdin accounts and then talked to his peers in his network to learn more about the individual. Only if he gets a favorable response will he be open to meeting with the applicant in person. He needs to hire within two weeks but has been unable, so far, to find a candidate who has the personality, drive, and work ethic he needs in his company.</p>
+			<p><strong>Goals:</strong> David is focusing on the priorities he has for hiring: Can he quickly find out if the candidate has the skills necessary to work both front-end and back-end development and can he easily navigate to the individuals github account to do so? After looking over the account, Greg will determine if this individual is worth his time to find out more about them through his networking circles and perhaps end with a face to face interview.</p>
+			<p><strong>User Story</strong> As a user, I need to be able to glance over the site to get an idea of the personality of the candidate, find their github link and linkdin account.</p>
+		</div>
+	</body>
+</html>
