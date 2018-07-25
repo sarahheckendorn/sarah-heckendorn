c7479c37801fa739d332bb4cc02260e9b1d11c8e
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-24 21:25:31 -0600

    added milestone2 and listed content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<p>For the first section you see, it will give my name and a one line sentence of my job. There will then be an 'about me' section with a professional headshot</br>
+			and a little blurb about myself and my skills as a web developer. It will also include links to my linkedin, github, and twitter accounts for easy accessibility and save</br>
+		potential employers time to just tap them right away.</p>
+	</br>
+		<p>On the second section, I will have a list of my portfolios with one image of the front page that links back to my websites. Below (or above, not decided for sure yet) I will</br>
+		have a list of the development languages that I know how to use.</p>
+	</br>
+		<p>
+			The third section will have a standard contact me form with links, again, to my github, linkedIn, and twitter. My reasoning for listing these twice on my mobile site specifically</br>
+			is so that it eliminates unnecessary scrolling to get those links again. I'm not set in stone on this one, but this is my reasoning behind it. I want my site to be fast and easy to use</br>
+			for potential employers who don't have a lot of time on their hands when looking at personal websites.
+		</p>
+	</body>
+</html>
