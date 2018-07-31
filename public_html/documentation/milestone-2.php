55298836220e518c805fb828f848a2c8c956145d
Sarah Heckendorn <praisinghim@jcangus.com>
2018-07-31 16:53:10 -0600

    Fixed typos and layout description

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,16 +6,15 @@
 	</head>
 	<body>
 		<h1>Wireframing for both mobile and desktop design</h1>
-		<p>For the first section you see, it will give my name and a one line sentence of my job. There will then be an 'about me' section with a professional headshot</br>
-			and a little blurb about myself and my skills as a web developer. It will also include links to my linkedin, github, and twitter accounts for easy accessibility and save</br>
+		<p>For the first section you see, it will give my name and a one line sentence of my job. There will then be an 'about me' section with a professional headshot
+			and a little blurb about myself and my skills as a web developer. It will also include links to my linkedin, github, and twitter accounts for easy accessibility and save
 		potential employers time to just tap them right away.</p>
 	</br>
-		<p>On the second section, I will have a list of my portfolios with one image of the front page that links back to my websites. Below (or above, not decided for sure yet) I will</br>
-		have a list of the development languages that I know how to use.</p>
+		<p>On the second section, I will have a list of the development languages that I know how to use. Below that, I will have a list of my portfolios with one image of the front page that links back to my websites.</p>
 	</br>
 		<p>
-			The third section will have a standard contact me form with links, again, to my github, linkedIn, and twitter. My reasoning for listing these twice on my mobile site specifically</br>
-			is so that it eliminates unnecessary scrolling to get those links again. I'm not set in stone on this one, but this is my reasoning behind it. I want my site to be fast and easy to use</br>
+			The third section will have a standard contact me form with links, again, to my github, linkedIn, and twitter. My reasoning for listing these twice on my mobile site specifically
+			is so that it eliminates unnecessary scrolling to get to those links again. I'm not set in stone on this one, but this is my reasoning behind it. I want my site to be fast and easy to use
 			for potential employers who don't have a lot of time on their hands when looking at personal websites.
 		</p>
 		<img src="Wireframemobile2.png">
