71fa25413bafe1461c9a68cd63b00a0ed92cd852
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-06 16:58:31 -0600

    Fleshed out layout of HTML code

---
+++ public_html/php/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
+<!--	/**header section-->
+	<div>
+		<div>
+			<img src="">
+		</div>
+		<di>
+			<h1>Sarah Heckendorn</h1>
+			<h3>Professional Web Developer | Creative</h3>
+		</di>
+<!--		/**about me and links**/-->
+		<div>
+			<div>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+			<di>
+				<h2>About Me</h2>
+				<p>FILL HERE!</p>
+			</di>
+			<div>
+				<h2>Check Out My:</h2>
+				<a href="https://github.com/sarahheckendorn">GitHub</a>
+				<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/">LinkedIn</a>
+				<a href="#">Twitter</a>
+			</div>
+		</div>
+<!--		/**Languages section-->
+		<div>
+			<h5>Languages</h5>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>Bootstrap</li>
+				<li>Angular</li>
+				<li>JQuery</li>
+				<li>JavaScript</li>
+				<li>PHP</li>
+				<li>mySQL</li>
+			</ul>
+		</div>
+<!--		/**Portfolio-->
+		<div>
+			<h4>Portfolio</h4>
+		</div>
+		<div>
+			<div>
+				<p>Lorum Ipsum</p>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+			<div>
+				<p>Lorum Ipsum</p>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+			<div>
+				<p>Lorum Ipsum</p>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+			<div>
+				<p>Lorum Ipsum</p>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+			<div>
+				<p>Lorum Ipsum</p>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+			<div>
+				<p>Lorum Ipsum</p>
+				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+			</div>
+		</div>
+<!--		/**Contact Me Section-->
+		<div>
+
+		</div>
+	</div>
+</html>
