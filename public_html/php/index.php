66d6f1ff17392343c4ebc2dcb58c8b7add04511f
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-07 14:18:01 -0600

    added sections for easier dividing of my code, also added html for the contact form

--- public_html/php/index.php
+++ public_html/php/index.php
@@ -7,23 +7,25 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
 <!--	/**header section-->
-	<div>
+	<section>
 		<div>
 			<img src="">
 		</div>
-		<di>
+		<div>
 			<h1>Sarah Heckendorn</h1>
 			<h3>Professional Web Developer | Creative</h3>
-		</di>
+		</div>
+	</section>
 <!--		/**about me and links**/-->
+	<section>
 		<div>
 			<div class="media">
 				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
 			</div>
-			<di>
+			<div>
 				<h2>About Me</h2>
 				<p>FILL HERE!</p>
-			</di>
+			</div>
 			<div>
 				<h2>Check Out My:</h2>
 				<a href="https://github.com/sarahheckendorn">GitHub</a>
@@ -31,7 +33,9 @@
 				<a href="#">Twitter</a>
 			</div>
 		</div>
+	</section>
 <!--		/**Languages section-->
+	<section>
 		<div>
 			<h5>Languages</h5>
 			<ul>
@@ -45,39 +49,58 @@
 				<li>mySQL</li>
 			</ul>
 		</div>
+	</section>
 <!--		/**Portfolio-->
+	<section>
 		<div>
 			<h4>Portfolio</h4>
 		</div>
 		<div>
-			<div>
-				<p>Lorum Ipsum</p>
-				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-			</div>
-			<div>
-				<p>Lorum Ipsum</p>
-				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-			</div>
-			<div>
-				<p>Lorum Ipsum</p>
-				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-			</div>
-			<div>
-				<p>Lorum Ipsum</p>
-				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-			</div>
-			<div>
-				<p>Lorum Ipsum</p>
-				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-			</div>
-			<div>
-				<p>Lorum Ipsum</p>
-				<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-			</div>
+			<p>Lorum Ipsum</p>
+			<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
 		</div>
-<!--		/**Contact Me Section-->
 		<div>
-
+			<p>Lorum Ipsum</p>
+			<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
 		</div>
-	</div>
+		<div>
+			<p>Lorum Ipsum</p>
+			<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</div>
+		<div>
+			<p>Lorum Ipsum</p>
+			<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</div>
+		<div>
+			<p>Lorum Ipsum</p>
+			<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</div>
+		<div>
+			<p>Lorum Ipsum</p>
+			<img src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</div>
+	</section>
+<!--		/**Contact Me Section-->
+	<section>
+		<div>
+			<form>
+				<h3>Contact Me!</h3>
+				<fieldset>
+					<input placeholder="Your name" type="text" tabindex="1" required autofocus>
+				</fieldset>
+				<fieldset>
+					<input placeholder="Your Email Address" type="email" tabindex="2" required>
+				</fieldset>
+				<fieldset>
+					<input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+				</fieldset>
+				<fieldset>
+					<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+				</fieldset>
+				<fieldset>
+					<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+				</fieldset>
+			</form>
+		</div>
+	</section>
 </html>
