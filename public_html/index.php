9c83d38181e11fbda03cc6c3d50498d8e0bff2ec
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-13 16:13:24 -0600

    added body tags

--- public_html/index.php
+++ public_html/index.php
@@ -7,118 +7,120 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="stylesheets/StylesheetSH.css">
 	</head>
-<!--	/**header section-->
-	<section class="container">
-		<div>
-			<h1>Sarah Heckendorn</h1>
-			<h3>Professional Web Developer | Creative</h3>
-		</div>
-	</section>
-<!--		/**about me and links**/-->
-	<section class="container-fluid">
-		<div class="row">
-			<div class="col-md-4">
-				<img class="image-fluid 15px" src="image/profileimage.jpg">
-			</div>
-			<div class="col-md-6">
-				<h2>About Me</h2>
-				<p>FILL HERE!</p>
-			</div>
+	<body class="background">
+	<!--	/**header section-->
+		<section class="container">
 			<div>
-				<h2>Check Out My:</h2>
-				<a href="https://github.com/sarahheckendorn">GitHub</a>
-				<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/">LinkedIn</a>
-				<a href="#">Twitter</a>
+				<h1>Sarah Heckendorn</h1>
+				<h3>Professional Web Developer | Creative</h3>
 			</div>
-		</div>
-	</section>
-<!--		/**Languages section-->
-	<section class="container-fluid">
-		<div class="row">
-			<h5>Languages</h5>
-			<div class="col-md-4">
-				<ul>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>Bootstrap</li>
-					<li>Angular</li>
-				</ul>
-			</div>
-			<div class="col-md-6">
-				<ul>
-					<li>JQuery</li>
-					<li>JavaScript</li>
-					<li>PHP</li>
-					<li>mySQL</li>
-				</ul>
-			</div>
-		</div>
-	</section>
-<!--		/**Portfolio-->
-	<section class="container-fluid px-4 py-4">
-		<div>
-			<h4>Portfolio</h4>
-		</div>
-		<div class="card-deck">
-			<div class="card">
-				<p class="card-title">Lorum Ipsum</p>
-				<div class="card-body">
-					<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</section>
+	<!--		/**about me and links**/-->
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-md-4">
+					<img class="image-fluid profile" src="image/profileimage.jpg">
+				</div>
+				<div class="col-md-6">
+					<h2>About Me</h2>
+					<p>FILL HERE!</p>
+				</div>
+				<div>
+					<h2>Check Out My:</h2>
+					<a href="https://github.com/sarahheckendorn">GitHub</a>
+					<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/">LinkedIn</a>
+					<a href="#">Twitter</a>
 				</div>
 			</div>
-			<div class="card">
-				<p class="card-title">Lorum Ipsum</p>
-				<div class="card-body">
-					<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</section>
+	<!--		/**Languages section-->
+		<section class="container-fluid">
+			<div class="row">
+				<h5>Languages</h5>
+				<div class="col-md-4">
+					<ul>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>Bootstrap</li>
+						<li>Angular</li>
+					</ul>
+				</div>
+				<div class="col-md-6">
+					<ul>
+						<li>JQuery</li>
+						<li>JavaScript</li>
+						<li>PHP</li>
+						<li>mySQL</li>
+					</ul>
 				</div>
 			</div>
-			<div class="card">
-				<p class="card-title">Lorum Ipsum</p>
-				<div class="card-body">
-					<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+		</section>
+	<!--		/**Portfolio-->
+		<section class="container-fluid px-4 py-4">
+			<div>
+				<h4>Portfolio</h4>
+			</div>
+			<div class="card-deck">
+				<div class="card">
+					<p class="card-title">Lorum Ipsum</p>
+					<div class="card-body">
+						<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+					</div>
+				</div>
+				<div class="card">
+					<p class="card-title">Lorum Ipsum</p>
+					<div class="card-body">
+						<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+					</div>
+				</div>
+				<div class="card">
+					<p class="card-title">Lorum Ipsum</p>
+					<div class="card-body">
+						<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+					</div>
+				</div>
+				<div class="card">
+					<p class="card-title">Lorum Ipsum</p>
+					<div class="card-body">
+						<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+					</div>
+				</div>
+				<div class="card">
+					<p class="card-title">Lorum Ipsum</p>
+					<div class="card-body">
+						<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+					</div>
+				</div>
+				<div class="card">
+					<p class="card-title">Lorum Ipsum</p>
+					<div class="card-body">
+						<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
+					</div>
 				</div>
 			</div>
-			<div class="card">
-				<p class="card-title">Lorum Ipsum</p>
-				<div class="card-body">
-					<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-				</div>
+		</section>
+	<!--		/**Contact Me Section-->
+		<section class="container">
+			<div>
+				<form>
+					<h3>Contact Me!</h3>
+					<fieldset>
+						<input placeholder="Your name" type="text" tabindex="1" required autofocus>
+					</fieldset>
+					<fieldset>
+						<input placeholder="Your Email Address" type="email" tabindex="2" required>
+					</fieldset>
+					<fieldset>
+						<input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+					</fieldset>
+					<fieldset>
+						<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+					</fieldset>
+					<fieldset>
+						<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+					</fieldset>
+				</form>
 			</div>
-			<div class="card">
-				<p class="card-title">Lorum Ipsum</p>
-				<div class="card-body">
-					<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-				</div>
-			</div>
-			<div class="card">
-				<p class="card-title">Lorum Ipsum</p>
-				<div class="card-body">
-					<img class="card-img-bottom w-100" src="http://absolute-respiratory.com/wp-content/uploads/2015/01/image-coming-soon.jpg">
-				</div>
-			</div>
-		</div>
-	</section>
-<!--		/**Contact Me Section-->
-	<section class="container">
-		<div>
-			<form>
-				<h3>Contact Me!</h3>
-				<fieldset>
-					<input placeholder="Your name" type="text" tabindex="1" required autofocus>
-				</fieldset>
-				<fieldset>
-					<input placeholder="Your Email Address" type="email" tabindex="2" required>
-				</fieldset>
-				<fieldset>
-					<input placeholder="Your Phone Number" type="tel" tabindex="3" required>
-				</fieldset>
-				<fieldset>
-					<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
-				</fieldset>
-				<fieldset>
-					<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-				</fieldset>
-			</form>
-		</div>
-	</section>
+		</section>
+	</body>
 </html>
