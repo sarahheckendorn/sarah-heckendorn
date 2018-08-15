4d0b5d37ec643efb4f64d9d46638c588688c5ad9
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-14 21:00:29 -0600

    addd class tags

--- public_html/index.php
+++ public_html/index.php
@@ -4,32 +4,33 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="stylesheets/StylesheetSH.css">
 	</head>
 	<body class="background">
 	<!--	/**header section-->
-		<section class="container aboutme d-block text-center">
+		<section class="container aboutme d-block text-center px-3 py-3 mx-5 py-3">
 			<div class="amtext">
 				<h1>Sarah Heckendorn</h1>
 				<h3>Professional Web Developer | Creative</h3>
 			</div>
 		</section>
 	<!--		/**about me and links**/-->
-		<section class="container-fluid">
+		<section class="container-fluid px-3 py-3">
 			<div class="row">
-				<div class="col-md-4">
-					<img class="image-fluid profile" src="image/profileimage.jpg">
+				<div class="col-md-2 px-2 mx-2">
+					<img class="image-fluid rounded-circle profile" src="image/profileimage.jpg">
 				</div>
 				<div class="col-md-6">
 					<h2>About Me</h2>
-					<p>FILL HERE!</p>
+					<p>Hey! My name is Sarah. I've graduated CNM Ingenuity's Deep Dive Coding Bootcamp and have learned more about coding than I thought possible in ten short weeks! I've been passionate about coding for the past two years, but was able to recently incorporate it more when I learned about Deep Dive. I thoroughly enjoy the whole coding process- backend to frontend! The problem solving, the creativity, all of it! I'm excited to jump into the tech world full time and launch my career in fullstack web development. Please have a look around, check out the links, and feel free to contact me!</p>
 				</div>
-				<div>
+				<div class="col-md-3 social">
 					<h2>Check Out My:</h2>
-					<a href="https://github.com/sarahheckendorn">GitHub</a>
-					<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/">LinkedIn</a>
-					<a href="#">Twitter</a>
+					<a href="https://github.com/sarahheckendorn"><i class="fab fa-github"></i></a>
+					<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/"><i class="fab fa-linkedin"></i></a>
+					<a href="#"><i class="fab fa-twitter-square text-size"></i></a>
 				</div>
 			</div>
 		</section>
@@ -90,24 +91,24 @@
 	<!--		/**Contact Me Section-->
 		<section class="container">
 			<div>
-				<form>
-					<h3>Contact Me!</h3>
-					<fieldset>
-						<input placeholder="Your name" type="text" tabindex="1" required autofocus>
-					</fieldset>
-					<fieldset>
-						<input placeholder="Your Email Address" type="email" tabindex="2" required>
-					</fieldset>
-					<fieldset>
-						<input placeholder="Your Phone Number" type="tel" tabindex="3" required>
-					</fieldset>
-					<fieldset>
-						<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
-					</fieldset>
-					<fieldset>
-						<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-					</fieldset>
-				</form>
+<!--				<form>-->
+<!--					<h3>Contact Me!</h3>-->
+<!--					<fieldset>-->
+<!--						<input placeholder="Your name" type="text" tabindex="1" required autofocus>-->
+<!--					</fieldset>-->
+<!--					<fieldset>-->
+<!--						<input placeholder="Your Email Address" type="email" tabindex="2" required>-->
+<!--					</fieldset>-->
+<!--					<fieldset>-->
+<!--						<input placeholder="Your Phone Number" type="tel" tabindex="3" required>-->
+<!--					</fieldset>-->
+<!--					<fieldset>-->
+<!--						<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>-->
+<!--					</fieldset>-->
+<!--					<fieldset>-->
+<!--						<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>-->
+<!--					</fieldset>-->
+<!--				</form>-->
 			</div>
 		</section>
 		<section>
