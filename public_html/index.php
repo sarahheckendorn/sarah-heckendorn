30a0c627fb43223d83e602cf2be251d455c44d46
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-15 13:10:41 -0600

    uncommented the form index for review

--- public_html/index.php
+++ public_html/index.php
@@ -91,24 +91,24 @@
 	<!--		/**Contact Me Section-->
 		<section class="container">
 			<div>
-<!--				<form>-->
-<!--					<h3>Contact Me!</h3>-->
-<!--					<fieldset>-->
-<!--						<input placeholder="Your name" type="text" tabindex="1" required autofocus>-->
-<!--					</fieldset>-->
-<!--					<fieldset>-->
-<!--						<input placeholder="Your Email Address" type="email" tabindex="2" required>-->
-<!--					</fieldset>-->
-<!--					<fieldset>-->
-<!--						<input placeholder="Your Phone Number" type="tel" tabindex="3" required>-->
-<!--					</fieldset>-->
-<!--					<fieldset>-->
-<!--						<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>-->
-<!--					</fieldset>-->
-<!--					<fieldset>-->
-<!--						<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>-->
-<!--					</fieldset>-->
-<!--				</form>-->
+				<form>
+					<h3>Contact Me!</h3>
+					<fieldset>
+						<input placeholder="Your name" type="text" tabindex="1" required autofocus>
+					</fieldset>
+					<fieldset>
+						<input placeholder="Your Email Address" type="email" tabindex="2" required>
+					</fieldset>
+					<fieldset>
+						<input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+					</fieldset>
+					<fieldset>
+						<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+					</fieldset>
+					<fieldset>
+						<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+					</fieldset>
+				</form>
 			</div>
 		</section>
 		<section>
