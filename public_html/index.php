350be918ba7a9f4dcd9f143dbee6f36d35f2aede
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-15 14:04:32 -0600

    added link icons for media sites

--- public_html/index.php
+++ public_html/index.php
@@ -114,9 +114,9 @@
 		<section>
 			<div class="d-xl-none d-lg-none d-md-none">
 				<h2>Easy Links:</h2>
-				<a href="https://github.com/sarahheckendorn">GitHub</a>
-				<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/">LinkedIn</a>
-				<a href="#">Twitter</a>
+				<a href="https://github.com/sarahheckendorn"><i class="fab fa-github px-2 py-4"></i></a>
+				<a href="https://www.linkedin.com/in/sarah-heckendorn-0721/"><i class="fab fa-linkedin px-2"></i></a>
+				<a href="#"><i class="fab fa-twitter-square px-2"></i></a>
 			</div>
 		</section>
 	</body>
