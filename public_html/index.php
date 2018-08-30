25aa2b4e720f5996e39c0d23936e2be9ad04a7b8
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-30 10:15:02 -0600

    updated gitignore file, added in script tags for jquery for contact form

--- public_html/index.php
+++ public_html/index.php
@@ -10,6 +10,9 @@
 		<link href="https://fonts.googleapis.com/css?family=Montserrat|Dancing+Script|Raleway" rel="stylesheet">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 	</head>
 	<body class="background">
 	<!--	/**header section-->
@@ -22,7 +25,7 @@
 		</section>
 	<!--		/**about me and links**/-->
 		<section id="aboutme" class="container font px-3 py-3">
-			<div class="">
+			<div class="d-flex">
 				<div class="col py-3">
 					<img id="profile" class="image rounded-circle" src="image/profileimage%20-%20Copy.jpg" alt="profile picture">
 					<h2 class="cursive">About Me</h2>
