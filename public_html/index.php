e4be1ddd49a35236d0720b9350d47709590c8f7a
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-06 09:19:38 -0600

    fixed scripts and readded necessary ones from boostrap to make the carousel work

--- public_html/index.php
+++ public_html/index.php
@@ -11,9 +11,10 @@
 		<link rel="stylesheet" href="stylesheets/stylesheet.css" type="text/css">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat|Dancing+Script|Raleway" rel="stylesheet">
 		<!--Jquery and popper-->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<!--scripts for form-->
 		<script src="js/validator.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
@@ -71,44 +72,46 @@
 		</table>
 	<!--		/**Portfolio-->
 		<div class="container">
-			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<div id="carousel" class="carousel slide" data-ride="carousel">
 				<ol class="carousel-indicators">
-					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					<li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+					<li data-target="#carouselIndicators" data-slide-to="1"></li>
+					<li data-target="#carouselIndicators" data-slide-to="2"></li>
 				</ol>
 <!--				will update the carousel as I have items to add. Have commented out code for ease so that I can easily add items as they come.-->
 				<div class="carousel-inner">
 					<div class="carousel-item active">
-						<img class="d-block w-100" src="image/image%20(2).jpg" alt="Image for bootstrap challenge">
-						<div class="carousel-caption d-none d-md-block">
-							<h5>Bootstrap Challenge</h5>
-							<p>Used a simple layout to practice styling with bootstrap and css. Completed during Deep Diving Coding Fullstack Bootcamp.</p>
+							<a href="" target="_blank">
+							<img class="d-block w-100" src="image/image%20(2).jpg" alt="Image for bootstrap challenge">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Bootstrap Challenge</h5>
+								<p>Used a simple layout to practice styling with bootstrap and css. Completed during Deep Diving Coding Fullstack Bootcamp.</p>
+							</div>
+							</a>
 						</div>
-					</div>
-					<div class="carousel-item">
-						<img class="d-block w-100" src="image/lake.jpg" alt="Image coming soon for NM-Outdoors Project">
-						<div class="carousel-caption d-none d-md-block">
-							<h5>NM-Outdoors</h5>
-							<p>A collaborative project with teammates from DDC Bootcamp. Implementing mySQL for backend databases, PHP for structuring the database, Api's for pulling data from a government site, Angular for putting the pieces together, and finally, bootstrap and css for styling.</p>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="image/lake.jpg" alt="Image coming soon for NM-Outdoors Project">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>NM-Outdoors</h5>
+								<p>A collaborative project with teammates from DDC Bootcamp. Implementing mySQL for backend databases, PHP for structuring the database, Api's for pulling data from a government site, Angular for putting the pieces together, and finally, bootstrap and css for styling.</p>
+							</div>
 						</div>
-					</div>
-					<div class="carousel-item">
-						<img class="d-block w-100" src="image/rustic-pictures.jpg?auto=yes&bg=555&fg=333" alt="Third slide">
-						<div class="carousel-caption d-none d-md-block">
-							<h5>Lorum Ipsum</h5>
-							<p>Description of slide and what work it was. Keep it brief but informative</p>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="image/rustic-pictures.jpg?auto=yes&bg=555&fg=333" alt="Third slide">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Lorum Ipsum</h5>
+								<p>Description of slide and what work it was. Keep it brief but informative</p>
+							</div>
 						</div>
-					</div>
+					<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
-				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
 			</div>
 		</div>
 	<!--		/**Contact Me Section-->
